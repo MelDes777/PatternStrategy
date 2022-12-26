e7c9b6c5582ec3ed43f7b4ba388029dd1e18a07f
Strakatov Nikolay <desux327@gmail.com>
2022-12-26 19:00:10 +0600

    Add project files.

---
+++ PatternStrategy/Program.cs
@@ -0,0 +1,12 @@
+﻿using PatternStrategy;
+using PatternStrategy.Strategies;
+
+Hero hero = new Hero("Squidward");
+
+hero.Attack();
+
+hero.SetWeapon(new Broom());
+
+hero.Attack();
+
+Console.ReadLine();
---
+++ PatternStrategy/Strategies/Plunger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternStrategy.Strategies
+{
+    internal class Plunger : IWeapon
+    {
+        void IWeapon.Shoot()
+        {
+            Console.WriteLine("attacks with a plunger");
+        }
+    }
+}
